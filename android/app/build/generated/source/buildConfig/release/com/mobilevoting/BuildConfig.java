efcad32ab23b1a66c80ebe12cf731287a927c3c9
Oginni Tosyn <tosyngy@gmail.com>
2018-12-18 07:58:41 +0100

    init done

---
+++ android/app/build/generated/source/buildConfig/release/com/mobilevoting/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mobilevoting;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.mobilevoting";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
